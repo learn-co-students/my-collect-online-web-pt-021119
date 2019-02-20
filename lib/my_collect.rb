de06514bb45af77adca137919ea39ed9f7598723
Levik Naparstek <levik010@gmail.com>
2019-02-20 19:03:15 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,30 @@
+def my_collect(array)
+  
+  # my_collect.collect do |languages|
+  #   my_collect.upcase
+    
+  # end
+    
+# i = 0 
+# collection = []
+# while i < array
+# yield array[i]
 
+# lang.upcase 
+# lang.upcase << collection
+ 
+# i += 1
+ 
+# end
+  i = 0
+  name_collection = []
+  while i < array.length
+    name_collection << yield(array[i]) 
+
+    i += 1
+  end
+  name_collection
+ 
+  
+  end 
 
